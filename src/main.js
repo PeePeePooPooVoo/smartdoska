e4b9a8c960dade80bc2b72cd00c5e77665fda72d
PeePeePooPooVoo <denberezinn@gmail.com>
2026-04-01 13:22:26 +0000

    Фикс ссылок

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { initRouter } from "./core/router.js";
 import { initUI } from "./core/uiContainer.js";
-import './supabaseClient.js'; // просто подключаем, чтобы window.supabase был определён
+import '/src/core/supabaseClient.js'; // просто подключаем, чтобы window.supabase был определён
 
 // Точка входа приложения
 function initApp() {
